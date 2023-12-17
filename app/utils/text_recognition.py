a4ade508bdd9ca14e8ab9fae4263b66d09ebed32
ShamHi <shomakakdela@gmail.com>
2023-12-17 17:40:20 +0300

    Easyocr commenting temp

--- app/utils/text_recognition.py
+++ app/utils/text_recognition.py
@@ -1,11 +1,12 @@
-from easyocr import Reader
+# from easyocr import Reader
+
+
+# def read_text(file):
+#     reader = Reader(['ru', 'en'])
+#     text = reader.readtext(file, detail=0, paragraph=True)
+#
+#     return text
 
 
 def read_text(file):
-    reader = Reader(['ru', 'en'])
-    text = reader.readtext(file, detail=0, paragraph=True)
-
-    return text
-
-
-
+    ...  # Temporarily
